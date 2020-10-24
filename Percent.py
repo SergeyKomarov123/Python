894c3be520443e51f4ccc27038b9380894ecc9fd
skomarov <skomarov@cloudfactory.ru>
2020-10-24 22:40:25 +0400

    Модульность

--- Percent.py
+++ Percent.py
@@ -1,92 +1,85 @@
 import random
 
-f = 1
-while (f == 1):
-    i = 0           # счетчик выхода из цикла
+print("=" * 60)
+print("Тест на уровень интуиции")   
+print("Программа будет по-очереди загадывать числовые значения.")
+print("Цель: постараться угадать загаданные числа.")
+
+playGame = bool(True)
+
+#f = 1
+while (playGame):
+    i = int(0)          # счетчик выхода из внутреннего цикла 
     l = 0           # счетчик удачных угадываний
     prs = int(0)    # коэфициент
     summ = 0        # переменная для суммироания процента из каждой итерации
-    area = 100      # верхняя граница диапазона
+    area = int(10)     # верхняя граница диапазона
+    iter = int(10)       # колличество повторов
 
-    print("Введите по очереди 5 чисел от \"1\" до", area)
-    while (i < 5):
-        target = random.randint(1, 100)
+    print("=" * 60)
+    print("Введите по очереди " + str(iter) +  " чисел в диапазоне от \"1\" до", area)
+    
+    for i in range(iter):
+        target = random.randint(1, area)
         g = int(0)
         while (g == 0):
             try:
-                a = int(input())
-                if (a > area or a < 1):
+                digit = int(input())
+                if (digit > area or digit < 1):
                     print("Необходимо ввести число от \"1\" до", area)
                     g = 0
                 else:
                     g = 1
             except:
-                print("Необходимо1111 ввести число от \"1\" до", area)
+                print("Необходимо ввести число от \"1\" до", area)
                 g = 0
-
+        
         rp = 0
         i += 1
-        # основная логика обработки вводимого числа
-        if (target >= a):
-            rp = target - a                     # находим разницу между загаданным и угаданным
-            rez = (rp * 100 / area)             # приводим эту разницу к единой шкале относительно area
-            itogo = 100 - rez                   # приводим к удобному проценту. Значение менее 80% - не принимается к вниманию ввиду высокой погрешности
-        if (target < a):
-            rp = a - target
-            rez = (rp * 100 / area)
-            itogo = 100 - rez
-        if (target == a):
+        rp = abs(target - digit)                    # находим разницу между загаданным и угаданным
+        rez = (rp * 100 / area)             # приводим эту разницу к единой шкале относительно area
+        itogo = 100 - rez
+        if (target == digit):
             l +=1               # считаем количество 100% совпадений
             prs += 5            # расчитываем коэфициент
         summ += itogo
 
-        # Для отладки, вывод промежуточного результата:
-        #print("=" * 40)
-        print("itogo", itogo, "%")
-        #print("l", l)
-        #print("summ", summ)
-        #print("=" * 40)
+        print("Загаданое число:", target, "Ваш ответ:", digit)
+        u = iter - i
+        if (u != 0):
+        	print("Осталось шагов:", u)
 
-    percent = int(summ / 5 + prs)
+    percent = int(summ / iter + prs)
+    ball = summ + prs
+    lp = (iter / 100 * l) * 100 #процент угадывания
 
     if (percent <= 50):
         pns = "крайне низкий"
-    if (percent >= 50 and percent < 65):
-        pns = "ниже среднестатистического"
-    if (percent >= 65 and percent < 85):
+    if (percent >= 50 and percent < 60):
+        pns = "ниже среднего"
+    if (percent >= 60 and percent < 80):
         pns = "в пределах среднестатистического"
-    if (percent >= 85 and percent < 90):
-        pns = "высокий"
+    if (percent >= 80 and percent < 90):
+        pns = "выше среднего"
     if (percent >= 90 and percent < 100):
-        pns = "крайне высокий"
+        pns = "высокий"
     if (percent >= 100):
-        pns = "Невероятный!"
-
-
-    print("*" * 40)
+        pns = "очень высокий"
+    if (lp >= 70):
+        pns = "запредельный"
+    if(lp >= 100):
+    	 pns = "забыли отключить чит мод?"
+    
+    print("*" * 60)
     print("РЕЗУЛЬТАТ:")
     print("Колличество полных совпадений:", l)
-    print("Уровень удачи:", str(pns) + "%")
-    print("*" * 40)
-
-
-    print("Сыграем еще раз? да/нет")
-    y = input()
-    if (y == "да"):
-        f = 1
-    if (y == "нет"):
-        f = 0
-    elif (y != "да" and y != "нет"):
-        n = 1
-        while (n == 1):
-            print("Введите \"да\" или \"нет\"")
-            y = input()
-            if (y == "нет"):
-                f = 0
-                n = 0
-            if (y == "да"):
-                f = 1
-                n = 0
+    print("Уровень вашей интуиции:", str(pns))
+    print("Начислено всего очков:", ball)
+    print("*" * 60)
+    if (input("Enter - играть еще, 0 - выход   ") == "0"):
+	    playGame = False
+    
+   
 print('=' * 40)
 print("Спасибо за игру!")
 print('=' * 40)
